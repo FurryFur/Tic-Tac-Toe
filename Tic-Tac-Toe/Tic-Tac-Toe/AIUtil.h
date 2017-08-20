c38a0b5af9560884cc41bf2225f3cd26f930b3af
Lance Chaney <lance.e.chaney@gmail.com>
2017-08-21 08:44:26 +1200

    Changed comments

--- Tic-Tac-Toe/Tic-Tac-Toe/AIUtil.h
+++ Tic-Tac-Toe/Tic-Tac-Toe/AIUtil.h
@@ -32,8 +32,8 @@ namespace AIUtil
 	/// The best action id, according to the Minimax algorithm, is returned in this variable.
 	/// Can take nullptr if no action id is required.
 	/// </param>
-	/// <param name="alpha">Highest (Best) utility seen by maximizer.</param>
-	/// <param name="beta">Lowest (Best) utility seen by minimizer.</param>
+	/// <param name="alpha">Highest (Best) utility seen so far by a max node on path to root.</param>
+	/// <param name="beta">Lowest (Best) utility seen so far by a min node on path to root.</param>
 	/// <returns>Utility of the current state.</returns>
 	int Minimax(IState& rState, size_t* pBestActionId, int alpha = std::numeric_limits<int>::min(), int beta = std::numeric_limits<int>::max());
 }
